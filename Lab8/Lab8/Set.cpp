9a78d5711eaa1740d407d00640d6b384d0a8c325
kmaaser <kmaaser@capital.edu>
2017-04-17 00:16:11 -0400

    addElement, removeElement, removeAllElements, containsElement, and isSubsetOf

--- Lab8/Lab8/Set.cpp
+++ Lab8/Lab8/Set.cpp
@@ -5,3 +5,49 @@
 #include <iostream>
 #include "Set.h"
 using namespace std;
+
+void Set::addElement(int x) {
+	_items[_numItems] = x;
+	++_numItems;
+}
+
+void Set::removeElement(int x) {
+	for (int i = 0; i < _numItems; ++i) {
+		if (x == _items[i]) {
+			for (int j = i + 1; j < _numItems; ++j) {
+				_items[j - 1] = _items[j];
+			}
+			_items[_numItems - 1] = 0;
+			--_numItems;
+		}
+	}
+}
+
+void Set::removeAllElements() {
+	for (int i = 0; i < _numItems; ++i) {
+		_items[i] = 0;
+	}
+	_numItems = 0;
+}
+
+bool Set::containsElement(int x) const {
+	for (int i = 0; i < _numItems; ++i) {
+		if (x == _items[i]) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+bool Set::isSubsetOf(const Set &other) const {
+	for (int i = 0; i < _numItems; ++i) {
+		if (other.containsElement(i)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
