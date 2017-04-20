eb717cf4279fd690c462542238746ecde39ef0ce
kmaaser <kmaaser@capital.edu>
2017-04-20 16:21:22 -0400

    Changes
    Initialize the constructor and varibale set to 0
    assert statements (some)
    fixd the operators

--- Lab8/Lab8/Set.cpp
+++ Lab8/Lab8/Set.cpp
@@ -6,18 +6,37 @@
 #include "Set.h"
 using namespace std;
 
+//Initalize constructor: Set
+//Initalizing _numItems to 0
+Set::Set() {
+	_numItems = 0;
+}
+
+//For loop goes through the array
+//If statement checks to see if 'x' is in the set
+//if x is in the set, then the return breaks out
+//after going through the for loop and checking to
+//see if x is in the set and it happens to be that
+//x is not in the set, then it is added to the set
+//in the next available spot
+//_numItems is also increased because now there is
+//another element added to the set
 void Set::addElement(int x) {
 	for (int i = 0; i < _numItems; ++i) {
 		if (x == _items[i]) {
 			return;
-		}
-		else {
-			_items[_numItems] = x;
-			++_numItems;
-		}
-	}	
+		}	
+	}
+	_items[_numItems] = x;
+	++_numItems;	
 }
 
+//For loop goes through the array and checks to see
+//if x is in the array. If x is in the array, then
+//go through the rest of the array
+//While doing this shift everything down 1 and overwriting 'x'.
+//Set the last element to 0 because it is no longer needed
+//and then finally decrement _numItems because of the shift
 void Set::removeElement(int x) {
 	for (int i = 0; i < _numItems; ++i) {
 		if (x == _items[i]) {
@@ -26,10 +45,14 @@ void Set::removeElement(int x) {
 			}
 			_items[_numItems - 1] = 0;
 			--_numItems;
+			break;
 		}
 	}
 }
 
+//For loop goes through the array and sets the elements
+//to 0, but all one really needs to do is set _numItems
+//to 0 because we don't care what is in the array anymore
 void Set::removeAllElements() {
 	for (int i = 0; i < _numItems; ++i) {
 		_items[i] = 0;
@@ -37,6 +60,10 @@ void Set::removeAllElements() {
 	_numItems = 0;
 }
 
+//For loop goes through the array to check values
+//If compares the values in the array to the value
+//we are looking for. If we find it, we return true.
+//If we don't find it, we return false
 bool Set::containsElement(int x) const {
 	for (int i = 0; i < _numItems; ++i) {
 		if (x == _items[i]) {
@@ -46,6 +73,13 @@ bool Set::containsElement(int x) const {
 	return false;
 }
 
+//Similar to containsElement
+//We use a for loop to go through the array (Set) and check
+//the values in the array
+//We use the containsElement to check and see if the subset
+//is or is not in the array
+//If the other set does not contain the items, we return false,
+//and if it goes through the loop, then we return true
 bool Set::isSubsetOf(const Set &other) const {
 	for (int i = 0; i < _numItems; ++i) {
 		if (!other.containsElement(_items[i])) {
@@ -55,22 +89,32 @@ bool Set::isSubsetOf(const Set &other) const {
 	return true;
 }
 
+//Create a new set so we can return it
+//Then add the items of set a to the new set
+//Second for loop to go thhrough set b and then
+//check to make sure the new set does not contain
+//any elements already in the new set then adds the
+//ones that are not
+//return the set
 Set operator+(const Set &a, const Set &b) {
-	Set set = Set();
+	Set set;
 	
 	for (int i = 0; i < a.numElements(); ++i) {
 		set.addElement(a._items[i]);
-		++set._numItems;
 	}
 	for (int j = 0; j < b.numElements(); ++j) {
 		if (!set.containsElement(b._items[j])) {
 			set.addElement(b._items[j]);
-			++set._numItems;
 		}
 	}
 	return set;
 }
 
+//Create new set
+//goes through set a and then checks to see if
+//set b doesnt contain the elemet of a
+//If it doesn't, it is added to the new set
+//return the set
 Set operator-(const Set &a, const Set &b) {
 	Set set;
 
@@ -82,9 +126,13 @@ Set operator-(const Set &a, const Set &b) {
 	return set;
 }
 
+//Goes through the array and adds them to a string
+//Also puts a comma inbetween the numbers except the
+//last one
+//return the os
 std::ostream& operator<<(std::ostream &os, const Set &a) {
 	for (int i = 0;i < a.numElements()-1;i++) {
-		os << a._items[i]+",";
+		os << a._items[i]+", ";
 	}
 	os << a._items[a.numElements() - 1];
 	return os;
--- Lab8/Lab8/main.cpp
+++ Lab8/Lab8/main.cpp
@@ -9,17 +9,48 @@ using namespace std;
 
 int main()
 {
-	/*Set a;
+	Set a;
 	Set b;
 	Set c;
-	int a[5] = { 1, 2, 3, 4 ,5 };
-	int b[2] = { 6, 7 };
-	int c[4] = { 1, 8, 3, 6 };
 
 	cout << "Test add element" << endl;
 	a.addElement(6);
-	assert(a.numElements == 6);
-	cout << "add element passed" << endl;*/
+	assert(a.numElements() == 1);
+	cout << "add element passed" << endl;
+
+	cout << "Test add element 2" << endl;
+	a.addElement(3);
+	a.addElement(6);
+	assert(a.numElements() == 2);
+	cout << "add element test 2 passed" << endl;
+
+	cout << "Test remove element" << endl;
+	a.removeElement(6);
+	assert(a.numElements() == 1);
+	cout << "remove element passed" << endl;
+
+	cout << "Test remove all elements" << endl;
+	a.removeAllElements();
+	assert(a.numElements() == 0);
+	cout << "remove all elements passed" << endl;
+
+	cout << "Test contains element" << endl;
+	a.addElement(6);
+	a.addElement(3);
+	a.containsElement(6);
+	assert(a.containsElement(6) == true);
+	cout << "contains elements passed" << endl;
+	
+	cout << "Test contains element 2" << endl;
+	a.containsElement(5);
+	assert(a.containsElement(5) == false);
+	cout << "contains element 2 test passed" << endl;
+
+	b.addElement(2);
+	b.addElement(3);
+
+	c = a + b;
+	assert(c.numElements() == 3);
 
 	//Set a;
 	//a.addElement(1);
